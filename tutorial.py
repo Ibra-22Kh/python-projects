6231f467947da8b00944d14708ac21b93bf891b1
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-15 21:44:20 +0300

    play around

---
+++ tutorial.py
@@ -0,0 +1,81 @@
+import random
+import turtle
+# s = 5 
+# print(s)
+
+# b ="hello"
+# print(b)
+
+# name = input("Enter your name")
+# age = input("Enter your age")
+
+# print("Name: " + name)
+# print("Age: " + age)
+
+
+# x = random.randint(0,100)
+# print("x: " , x)
+
+skillpoints = 15
+# if skillpoints < 16:
+#     print("You have less points")
+# else:
+#     print("You have more points")
+
+# shoppingCarts =  []
+# item = input("Enter the name of the product")
+# shoppingCarts.append(item)
+# print("list", shoppingCarts)
+
+
+#
+#Finite State Machine 
+
+# state = 1
+# while(1):
+#     transit = input("Enter your alphabet")
+#     print("you have entered the state:",state)
+#     if state == 1:
+#         if transit == 'W':
+#             state = 2
+
+#     elif state == 2:
+#         if transit == 'G':
+#             state = 3
+#         elif transit == 'S':
+#             state = 1
+    
+#     elif state == 3:
+#         if transit == 'L':
+#             state = 1
+
+#     print("maintenent tu es le state ici:",state)
+
+
+l = ["apple","peer","mango","orange"]
+
+for elment in l:
+    print(elment)
+
+array=[1,23,4,56,7,8,9]
+
+for element in array:
+    print(element+2)
+
+# #Square
+# farouq_turtle = turtle.Turtle()
+
+
+
+# def square():
+#   farouq_turtle.forward(100)
+#   farouq_turtle.right(90)
+#   farouq_turtle.forward(100)
+#   farouq_turtle.right(90)
+#   farouq_turtle.forward(100)
+#   farouq_turtle.right(90)
+#   farouq_turtle.forward(100)
+
+# square()
+# farouq_turtle.forward(100)
+# square()
